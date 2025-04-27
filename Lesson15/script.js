cae26f13a13ad833eedfc22d1a0bea9ff2a6e3d0
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-04-27 20:36:00 +0530

    creatd the all duntions as the print , get new barcodes as like that I think i have done it good ❤ and this is working well

--- Lesson15/script.js
+++ Lesson15/script.js
@@ -1,1 +1,23 @@
-JsBarcode("#barcode", "Genarate Barcode Now");
+JsBarcode("#barcode", "Genarate your barcode");
+
+// HERE I NEED TO WRITE WHEN CLICK THE SUBMIT BUTTON WHAT HAPPEN 
+document.getElementById("submit").addEventListener('click', function(event){
+    //SUBMITED
+    event.preventDefault();
+    let val = document.getElementById("bar_Value").value;
+    if(val !== ""){
+        JsBarcode("#barcode", val);
+    }
+})
+
+document.getElementById("print").addEventListener('click', function(){
+    //PRINT BUTTON CLICKED
+    document.getElementById("textwrapper").style.display='none';
+
+    document.getElementById("tag").style.display='block';
+})
+
+
+document.querySelector("body").addEventListener('keypress',function(){
+    document.getElementById("textwrapper").style.display='block';
+})
